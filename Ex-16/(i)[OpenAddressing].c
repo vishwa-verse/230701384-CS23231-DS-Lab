c31106d5b6b83550ae56a05b8ee2f84ef5b9e75d
vishwa-230701384 <230701384@rajalakshmi.edu.in>
2024-06-15 21:01:03 +0530

    -

---
+++ Ex-16/(i)[OpenAddressing].c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+typedef struct {
+    int key;
+    int value;
+} Entry;
+
+Entry hashTable[SIZE];
+
+void initializeHashTable() {
+    for (int i = 0; i < SIZE; i++) {
+        hashTable[i].key = -1;
+        hashTable[i].value = -1;
+    }
+}
+
+int hashFunction(int key) {
+    return key % SIZE;
+}
+
+void insert(int key, int value) {
+    int index = hashFunction(key);
+
+    while (hashTable[index].key != -1) {
+        index = (index + 1) % SIZE;
+    }
+
+    hashTable[index].key = key;
+    hashTable[index].value = value;
+}
+
+int search(int key) {
+    int index = hashFunction(key);
+
+    while (hashTable[index].key != -1) {
+        if (hashTable[index].key == key) {
+            return hashTable[index].value;
+        }
+        index = (index + 1) % SIZE;
+    }
+
+    return -1;
+}
+
+void displayHashTable() {
+    printf("Hash Table:\n");
+    printf("-----------\n");
+    for (int i = 0; i < SIZE; i++) {
+        if (hashTable[i].key != -1) {
+            printf("Index %d: Key = %d, Value = %d\n", i, hashTable[i].key, hashTable[i].value);
+        }
+    }
+}
+
+int main() {
+    initializeHashTable();
+
+    insert(5, 10);
+    insert(15, 20);
+    insert(25, 30);
+    insert(35, 40);
+
+    displayHashTable();
+
+    int key = 15;
+    int value = search(key);
+    if (value != -1) {
+        printf("Value for key %d: %d\n", key, value);
+    } else {
+        printf("Key %d not found\n", key);
+    }
+
+    return 0;
+}
